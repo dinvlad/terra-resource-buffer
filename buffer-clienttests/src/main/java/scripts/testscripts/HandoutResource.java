32cb0e0b70772b19e9cade5a2164c30711aeea9a
yonghao <yyhao1@gmail.com>
2020-12-03 20:58:26 -0500

    add perf test on RBS

--- buffer-clienttests/src/main/java/scripts/testscripts/HandoutResource.java
+++ buffer-clienttests/src/main/java/scripts/testscripts/HandoutResource.java
@@ -3,6 +3,7 @@ package scripts.testscripts;
 import bio.terra.buffer.api.BufferApi;
 import bio.terra.buffer.client.ApiClient;
 import bio.terra.buffer.client.ApiException;
+import bio.terra.buffer.model.CloudResourceUid;
 import bio.terra.buffer.model.PoolInfo;
 import bio.terra.testrunner.runner.TestScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
@@ -10,11 +11,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scripts.utils.BufferServiceUtils;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static scripts.utils.BufferServiceUtils.*;
+
+/** A {@link TestScript} which request X projects from Buffer Services then verify pool can be refilled after Y hours.*/
 public class HandoutResource extends TestScript {
   private static final Logger logger = LoggerFactory.getLogger(HandoutResource.class);
+
+  /** How many projects to get for from Buffer Service. */
+  private static final Integer RESOURCE_COUNT = 1000;
 
   /** Public constructor so that this class can be instantiated via reflection. */
   public HandoutResource() {
@@ -25,19 +41,25 @@ public class HandoutResource extends TestScript {
   public void userJourney(TestUserSpecification testUser) throws Exception {
     ApiClient apiClient = BufferServiceUtils.getClient(server);
     BufferApi bufferApi = new BufferApi(apiClient);
-
-    // TODO: is there a valid pool id that I could use here instead of expecting failure?
-    try {
-      PoolInfo poolInfo = bufferApi.getPoolInfo("123");
-      logger.debug("poolInfo: {}", poolInfo);
-      assertThat("GET pool info did not throw not found exception", false);
-    } catch (ApiException apiEx) {
-      logger.debug("Caught exception fetching pool info", apiEx);
-      assertThat("Exception text reason", apiEx.getResponseBody().contains("Pool 123 not found"));
+    Set<String> handoutRequestIds = new HashSet<>();
+    for(int i = 0; i < RESOURCE_COUNT; i ++) {
+      String handoutRequestId = UUID.randomUUID().toString();
+      logger.debug("Generated handoutRequestId: {}", handoutRequestId);
+      try {
+      String projectId = bufferApi.handoutResource(POOL_ID, handoutRequestId).getCloudResourceUid().getGoogleProjectUid().getProjectId();
+      logger.debug("Iteration: {}, project Id: {} for handoutRequestId: {}", i, projectId, handoutRequestId);
+      assertThat(handoutRequestIds, not(hasItem(projectId)));
+      handoutRequestIds.add(projectId);
+      } catch (ApiException apiEx) {
+        logger.debug("Caught exception requesting resource, handoutRequestId: {}", handoutRequestId, apiEx);
+      }
     }
-
-    int httpCode = bufferApi.getApiClient().getStatusCode();
-    logger.info("GET pool info HTTP code: {}", httpCode);
-    assertThat(httpCode, equalTo(404));
+    logger.debug("Done requesting {} number of resources", handoutRequestIds.size());
+    // For now we verifies all RESOURCE_COUNT calls successfully. Not sure that is too ideal or we want to set some
+    // threshold like we allow 0.1% failure rate is allowable in this burst case.
+    assertThat(handoutRequestIds.size(), equalTo(RESOURCE_COUNT));
+    PoolInfo poolInfo = pollUntilPoolFull(bufferApi, Duration.ofHours(2));
+    assertThat(poolInfo.getResourceStateCount().get("CREATING"), equalTo(0));
+    assertThat(poolInfo.getResourceStateCount().get("READY"), equalTo(POOL_SIZE));
   }
 }
